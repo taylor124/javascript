ed67ed49340c0389ea74d7bdb7750f916583fe57
taylor124 <74930599+taylor124@users.noreply.github.com>
2020-12-15 16:20:48 -0500

    java testing

--- java.js
+++ java.js
@@ -1,1 +1,9 @@
 var RobotGladiators = window.confirm("Welcome to Robot Gladiators, Shall we begin?")
+
+if(RobotGladiators === true){
+    window.alert("Game on!")
+}
+
+else{
+    window.alert("Farewell then.")
+}
